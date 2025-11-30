dc34d22bcd912c89f077d706b1c9a1c49698d6e0
miftahr149 <miftahr149@gmail.com>
2025-11-30 11:23:06 +0800

    modify the controller

--- mental-health-literacy-hub/src/main/java/my/utm/mentalhealthapp/controller/MentalHealthChallengeController.java
+++ mental-health-literacy-hub/src/main/java/my/utm/mentalhealthapp/controller/MentalHealthChallengeController.java
@@ -2,16 +2,40 @@ package my.utm.mentalhealthapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.http.HttpSession;
+
+import my.utm.mentalhealthapp.model.MentalHealthChallenge;
 
 @Controller
 @RequestMapping("/mental-health-challenge")
 public class MentalHealthChallengeController {
 
   @GetMapping("/")
-  public ModelAndView home() {
+  public ModelAndView home(HttpSession session) {
     ModelAndView modelAndView = new ModelAndView("mental_health_challenge");
+    if (session.getAttribute("userRole") == null) {
+      modelAndView.setViewName("redirect:/mental-health-challenge/demo");
+      return modelAndView;
+    }
+
+    modelAndView.addObject("challenges", MentalHealthChallenge.getAll());
     return modelAndView;
+  }
+
+  @GetMapping("/demo")
+  public ModelAndView demo() {
+    ModelAndView modelAndView = new ModelAndView("demo_choose_role");
+    return modelAndView;
+  }
+
+  @PostMapping("/demo")
+  public String handleDemoRoleSelection(@RequestParam(name = "role", required = true) String role,
+      HttpSession session) {
+    session.setAttribute("userRole", role);
+    return "redirect:/mental-health-challenge/";
   }
 }

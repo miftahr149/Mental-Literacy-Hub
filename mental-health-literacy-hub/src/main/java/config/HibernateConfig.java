88cac03ae3b694e254e66caf736933f334eabc8e
miftahr149 <miftahr149@gmail.com>
2026-01-05 17:13:32 +0800

    Create HibernateConfig.java

---
+++ mental-health-literacy-hub/src/main/java/config/HibernateConfig.java
@@ -0,0 +1,51 @@
+package config;
+
+import java.util.Properties;
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScan(basePackages = {"my.utm.mentalhealthapp.service", "my.utm.mentalhealthapp.entity"})
+public class HibernateConfig {
+    private static String MYSQL_USERNAME = "root";
+    private static String MYSQL_PASSWORD = "admin123";
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/mentalhealth_db");
+
+        dataSource.setUsername("root");
+        dataSource.setPassword("admin123");
+
+        return dataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+        sessionFactoryBean.setDataSource(this.dataSource());
+        sessionFactoryBean.setPackagesToScan("my.utm.mentalhealthapp.entity");
+
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+        hibernateProperties.setProperty("hibernate.show_sql", "true");
+        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
+
+        sessionFactoryBean.setHibernateProperties(hibernateProperties);
+        return sessionFactoryBean;
+    }
+
+    @Bean
+    public HibernateTransactionManager transactionManager(LocalSessionFactoryBean sessionFactory) {
+        return new HibernateTransactionManager(sessionFactory.getObject());
+    }
+}
